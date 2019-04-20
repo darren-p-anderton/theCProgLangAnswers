a930addc34dfd44f2cd6155a99212db6528f37cd
Darren P Anderton <household@dpanderton.com>
2019-04-19 22:34:43 -0400

    Added  header for chapter excercise function declarations and excercise 1.3

---
+++ theCProgLangAnswers/Ch1Headers.h
@@ -0,0 +1,8 @@
+#ifndef CH1HEADERS_H_
+#define CH1HEADERS_H_
+
+void Ch1Ex3();
+
+#endif
+
+
--- theCProgLangAnswers/Ex1-1,2.c
+++ theCProgLangAnswers/Ex1-1,2.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+#include "Ch1Headers.h"
 
 main()
 {
 	printf("hello, world\n");
+	
+	Ch1Ex3();
+
 }
 
-//Darren P. Anderton
+/*Darren P. Anderton*/
